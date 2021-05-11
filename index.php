23388a9080d82c4dc99a0b6d248349b01de23484
lilitabrahamyan <lilitdev123@gmail.com>
2021-05-11 13:21:19 +0400

    index file changed, result now shows after sending request. Fixed input, now supports float

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 require('Test.php');
 require_once('SMath.php');
 
-// echo $_REQUEST;
-
 echo('
-<form method="post"><br>
-First number:<input type="number" name="name"><br>
-Second Number:<input type="number" name="name"><br>
+<form action="index.php" method="post"><br>
+First number:<input type="number" name="first_number"step="0.001"><br>
+Second Number:<input type="number" name="second_number"step="0.001"><br>
 <input type="submit" name="submit" value="Count sum"><br>
-</form>');
+</form>
+');
 
+if ($_REQUEST != null){
+    echo 'Result is '.Math::Sum($_REQUEST['first_number'], $_REQUEST['second_number']);
+}    
 
 ?>
